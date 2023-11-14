8c52aca489267a2ca7e683ec79eef0e646f15c7b
JuankaEsMa <joancarles.escobar@gmail.com>
2023-11-14 20:28:02 +0100

    fotos deploy railway

--- UD26-PiezasYProveedores/src/main/java/com/example/demo/dao/IProveedorDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.dto.Proveedor;
-
-public interface IProveedorDAO extends JpaRepository<Proveedor,String>{
-
-}
